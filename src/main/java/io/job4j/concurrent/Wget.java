4536c248f295578cc480ed2c47e9cf637c3a093a
Капустин Дмитрий <onewaykd@gmail.com>
2024-09-23 10:57:49 +0300

    4. Режим ожидания.

---
+++ src/main/java/io/job4j/concurrent/Wget.java
@@ -0,0 +1,21 @@
+package io.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        System.out.println("Start loading ... ");
+                        for (int i = 1; i <= 100; i++) {
+                            System.out.print("\rLoading : " + i + "%");
+                            Thread.sleep(10);
+                        }
+                        System.out.println("\nLoaded");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+    }
+}
