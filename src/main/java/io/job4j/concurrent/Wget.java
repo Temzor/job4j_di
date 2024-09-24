f04c29a416d9bbe7365b5ec135b5b9a58fae9bd2
Dmitrii <46348969+Temzor@users.noreply.github.com>
2024-09-24 10:14:13 +0300

    8. Скачивание файла с ограничением

--- src/main/java/io/job4j/concurrent/Wget.java
+++ src/main/java/io/job4j/concurrent/Wget.java
@@ -1,21 +1,52 @@
 package io.job4j.concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        System.out.println("Start loading ... ");
-                        for (int i = 1; i <= 100; i++) {
-                            System.out.print("\rLoading : " + i + "%");
-                            Thread.sleep(1000);
-                        }
-                        System.out.println("\nLoaded");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        var startAt = System.currentTimeMillis();
+        var file = new File("tmp.xml");
+        try (var input = new URL(url).openStream();
+             var output = new FileOutputStream(file)) {
+            System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
+            var dataBuffer = new byte[speed];
+            int bytesRead;
+            while ((bytesRead = input.read(dataBuffer, 0, dataBuffer.length)) != -1) {
+                var downloadAt = System.nanoTime();
+                output.write(dataBuffer, 0, bytesRead);
+                var time = System.nanoTime() - downloadAt;
+                if (time < 1_000_000) {
+                    Thread.sleep((1_000_000 - time) / 1000);
                 }
-        );
-        thread.start();
+                System.out.println("Read " + bytesRead + " bytes : " + (System.nanoTime() - downloadAt) + " nano.");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println(Files.size(file.toPath()) + " bytes");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
